dcadb2db9a8892f6f80df5820a4abbc791080515
Vitaly Khan <swng69@yandex.ru>
2020-02-26 10:26:47 +0800

    Do HW03 3

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,5 +1,13 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -10,28 +18,65 @@ import java.util.List;
 @Repository
 public class JdbcMealRepository implements MealRepository {
 
+    private final JdbcTemplate template;
+
+    private final NamedParameterJdbcTemplate npjTemplate;
+
+    private final SimpleJdbcInsert sjInsert;
+
+    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    @Autowired
+    public JdbcMealRepository(JdbcTemplate template, NamedParameterJdbcTemplate npjTemplate) {
+        this.template = template;
+        this.npjTemplate = npjTemplate;
+        this.sjInsert = new SimpleJdbcInsert(template)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource sqlSource = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("user_id", userId)
+                .addValue("datetime", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+        if (meal.isNew()) {
+            Number id = sjInsert.executeAndReturnKey(sqlSource);
+            meal.setId(id.intValue());
+        } else if (npjTemplate.update("UPDATE meals SET " +
+                "user_id=:user_id, " +
+                "datetime=:datetime, " +
+                "description=:description, " +
+                "calories=:calories " +
+                "WHERE id=:id AND user_id=:user_id", sqlSource) == 0) {
+            return null;
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return template.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = template.query("SELECT * FROM meals WHERE id=? AND user_id=?", ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return template.query("SELECT * FROM meals WHERE user_id=? ORDER BY datetime DESC", ROW_MAPPER, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return template.query(
+                "SELECT * FROM meals WHERE user_id=? AND datetime>=? AND datetime<? ORDER BY datetime DESC",
+                ROW_MAPPER, userId, startDate, endDate);
     }
 }
