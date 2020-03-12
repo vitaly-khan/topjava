203d63ea4af91d2e821b1e3fe7b473a79ca45d99
Vitaly Khan <swng69@yandex.ru>
2020-03-12 18:20:23 +0800

    Do HW05 Optional 5

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryForHsqlDb.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.HSQL_DB)
+public class JdbcMealRepositoryForHsqlDb extends JdbcMealRepository<Timestamp> {
+
+    @Autowired
+    public JdbcMealRepositoryForHsqlDb(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected Timestamp getSpecificDateTime(LocalDateTime dateTime) {
+        return dateTime == null ? null : Timestamp.valueOf(dateTime);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryForPostgreSql.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.POSTGRES_DB)
+public class JdbcMealRepositoryForPostgreSql extends JdbcMealRepository<LocalDateTime> {
+
+    @Autowired
+    public JdbcMealRepositoryForPostgreSql(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected LocalDateTime getSpecificDateTime(LocalDateTime dateTime) {
+        return dateTime;
+    }
+}
