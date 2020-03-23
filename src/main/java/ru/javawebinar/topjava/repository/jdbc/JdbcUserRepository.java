526bd8cf8729a48b587c898c3adbb5dfabf7041f
Vitaly Khan <swng69@yandex.ru>
2020-03-23 16:26:01 +0800

    Do HW06 2.3&2.4 Optional

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,24 +1,61 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
-    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+    private static UserResultSetExtractor urse = new UserResultSetExtractor();
+
+    private static class UserResultSetExtractor implements ResultSetExtractor<List<User>> {
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            List<User> list = new ArrayList<>();
+            while (rs.next()) {
+                int currentUserId = rs.getInt("id");
+                User user = list.stream().filter(x -> x.getId() == currentUserId).findFirst().orElse(null);
+                Role role = Role.valueOf(rs.getString("role"));
+
+                if (user == null) {
+                    user = new User(currentUserId,
+                            rs.getString("name"),
+                            rs.getString("email"),
+                            rs.getString("password"),
+                            rs.getInt("calories_per_day"),
+                            rs.getBoolean("enabled"),
+                            rs.getDate("registered"),
+                            EnumSet.of(role));
+                    list.add(user);
+                } else {
+                    user.getRoles().add(role);
+                }
+            }
+            return list;
+        }
+    }
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -39,16 +76,33 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     @Transactional
     public User save(User user) {
-        BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
 
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update(
-                "UPDATE users SET name=:name, email=:email, password=:password, " +
-                        "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
-            return null;
+
+        } else {
+            if (namedParameterJdbcTemplate.update(
+                    "UPDATE users SET name=:name, email=:email, password=:password, registered=:registered, " +
+                            "enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
+                return null;
+            }
+            jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
         }
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO user_roles (user_id, role) values(?,?)",
+                new BatchPreparedStatementSetter() {
+
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, user.getId());
+                        ps.setString(2, new ArrayList<>(user.getRoles()).get(i).name());
+                    }
+
+                    public int getBatchSize() {
+                        return user.getRoles().size();
+                    }
+                });
         return user;
     }
 
@@ -60,19 +114,21 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        List<User> list = jdbcTemplate.query("SELECT * FROM users JOIN user_roles " +
+                "ON users.id = user_roles.user_id WHERE users.id=?", urse, id);
+        return DataAccessUtils.singleResult(list);
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        List<User> list = jdbcTemplate.query("SELECT * FROM users JOIN user_roles " +
+                "ON users.id = user_roles.user_id WHERE email=?", urse, email);
+        return DataAccessUtils.singleResult(list);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query("SELECT * FROM users JOIN user_roles " +
+                "ON users.id = user_roles.user_id ORDER BY name, email", urse);
     }
 }
